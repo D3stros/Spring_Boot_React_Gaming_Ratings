3f509e56652d7ae5bfb2856ce11ab7ba999be955
D3stros <patrick.fallmann@hotmail.com>
2020-07-11 21:25:22 +0200

    Created spring boot file structure with Spring Initializr

---
+++ src/main/java/com/gamingratings/gamingratings/GamingRatingsApplication.java
@@ -0,0 +1,13 @@
+package com.gamingratings.gamingratings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingRatingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingRatingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamingratings/gamingratings/GamingRatingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamingratings.gamingratings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingRatingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
