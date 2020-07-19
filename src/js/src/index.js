0c53f07c4a31b518733e92d5cbeb8c523a2bf8eb
D3stros <patrick.fallmann@hotmail.com>
2020-07-19 16:24:24 +0200

    Reimplemented strict mode in index.js

--- src/js/src/index.js
+++ src/js/src/index.js
@@ -7,9 +7,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React>
+  <React.StrictMode>
     <App />
-  </React>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
